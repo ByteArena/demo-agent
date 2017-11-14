b8fb87075f967e0188be0c9d2263837a6244afaf
Sven SAULEAU <sven.sauleau@xtuc.fr>
2017-11-14 16:07:21 +0100

    fix: update sdk

--- src/index.js
+++ src/index.js
@@ -4,23 +4,23 @@ const computeAvoidanceForce = require('./computeAvoidanceForce');
 
 const agent = comm.connect()
 
-agent.on('perception', (perception, sendMutations) => {
+agent.on('perception', perception => {
     const avoidanceForce = computeAvoidanceForce(perception);
     const speed = perception.specs.maxspeed;
     const seekingForce = new Vector2(Math.random() * 30 * (Math.random() > .5 ? -1 : 1), Math.random() * 30 * (Math.random() > .5 ? -1 : 1));
 
-    const moves = [];
+    const actions = [];
 
     steering = (new Vector2())
         .add(avoidanceForce.clone().mag(100))
         .add(seekingForce.clone().mag(1))
         .mag(speed);
 
-    moves.push({ Method: 'steer', Arguments: steering.toArray(5) });
-    moves.push({ Method: 'shoot', Arguments: [0, 10] });
+    actions.push({ Method: 'steer', Arguments: steering.toArray(5) });
+    actions.push({ Method: 'shoot', Arguments: [0, 10] });
 
     // Pushing batch of mutations
-    sendMutations(moves)
+    agent.takeActions(actions)
 })
 
 agent.on('specs', (x) => console.log(x))
