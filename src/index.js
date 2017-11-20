f4905b00401fb130184bc0f80fa37acd87394a9e
Jérôme Schneider / Net Gusto <contact@netgusto.com>
2017-11-20 23:57:25 +0100

    Shooting !

--- src/index.js
+++ src/index.js
@@ -1,28 +1,37 @@
-const { vector: { Vector2 }, comm } = require('bytearena-sdk');
+const { vector: { Vector2 }, comm } = require("bytearena-sdk");
 
-const computeAvoidanceForce = require('./computeAvoidanceForce');
+const computeAvoidanceForce = require("./computeAvoidanceForce");
 
 let specs = null;
 
 const agent = comm.connect();
 
-agent.on('perception', perception => {
+agent.on("perception", perception => {
     const avoidanceForce = computeAvoidanceForce(perception, specs);
     const speed = specs.maxspeed;
-    const seekingForce = new Vector2(Math.random() * 30 * (Math.random() > .5 ? -1 : 1), Math.random() * 30 * (Math.random() > .5 ? -1 : 1));
+    const seekingForce = new Vector2(
+        Math.random() * 30 * (Math.random() > 0.5 ? -1 : 1),
+        Math.random() * 30 * (Math.random() > 0.5 ? -1 : 1)
+    );
 
     const actions = [];
 
-    steering = (new Vector2())
+    for (const otheragentkey in perception.vision) {
+        const otheragent = perception.vision[otheragentkey];
+        if (otheragent.tag !== "agent") continue;
+
+        actions.push({ Method: 'shoot', Arguments: Vector2.fromArray(otheragent.center).add(Vector2.fromArray(otheragent.velocity)).toArray(5) });
+    }
+
+    steering = new Vector2()
         .add(avoidanceForce.clone().mag(100))
         .add(seekingForce.clone().mag(1))
         .mag(speed);
 
-    actions.push({ Method: 'steer', Arguments: steering.toArray(5) });
-    actions.push({ Method: 'shoot', Arguments: [0, 10] });
+    actions.push({ Method: "steer", Arguments: steering.toArray(5) });
 
     // Pushing batch of mutations
-    agent.takeActions(actions)
-})
+    agent.takeActions(actions);
+});
 
-agent.on('welcome', _specs => (specs = _specs));
+agent.on("welcome", _specs => (specs = _specs));
